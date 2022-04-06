bc735cc0f4435e4a3cbeed547b944c2aef35a562
What about KSCH <54140748+KSCHdsc@users.noreply.github.com>
2022-04-06 19:04:04 +0200

    Create startdestrucord.bat

---
+++ startdestrucord.bat
@@ -0,0 +1,5 @@
+@echo off
+
+if exist "%UserProfile%\AppData\Local\xDestruCord\Destrucord.exe" start %UserProfile%\AppData\Local\xDestruCord\Destrucord.exe
+
+if exist "%UserProfile%\AppData\Roaming\xDestruCord\Destrucord.exe" start %UserProfile%\AppData\Roaming\xDestruCord\Destrucord.exe
